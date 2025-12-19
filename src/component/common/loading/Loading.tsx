d4e2e4561accf533e485c42f08e7f67f8e562d16
AmirHashemi77 <amirhashemi539@gmail.com>
2025-12-19 21:53:34 +0330

    change loading color

--- src/component/common/loading/Loading.tsx
+++ src/component/common/loading/Loading.tsx
@@ -1,6 +1,6 @@
 export default function LoadingScreen() {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black/80 z-50 w-full h-full">
+    <div className="fixed inset-0 flex items-center justify-center bg-black z-50 w-full h-full">
       <div className="w-16 h-16 rounded-full animate-spin border-8 border-solid border-primary-foreground border-t-transparent shadow-md"></div>
     </div>
   );
